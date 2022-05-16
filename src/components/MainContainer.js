98cf24a76daf7bc6561b8e8d5d1ba0ed8e575b66
annomao <omaoannkerubo@gmail.com>
2022-05-17 01:09:07 +0300

    Completed assignment

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([])
+  const [portfolio, setPortfolio] = useState([])
+  const [sortMethod, setSortMethod] = useState("")
+  const [filter, setFilter] = useState("Tech")
+
+  useEffect(()=>{
+    fetch(`http://localhost:3001/stocks?type=${filter}`)
+    .then(res => res.json())
+    .then(data => setStocks(data))
+  },[filter])
+
+  const sorting = (stocks) => {
+
+    if(sortMethod==="Price"){
+      return stocks.sort((firstItem, secondItem) => firstItem.price - secondItem.price)
+    }else if(sortMethod==="Alphabetically"){
+      return stocks.sort((firstItem, secondItem) => {
+        return firstItem.ticker.localeCompare(secondItem.ticker); })
+    }else{
+      return stocks
+    }
+  }
+   const buyStock = (boughtStock) => setPortfolio([...portfolio,boughtStock]) 
+   const sellStock = (soldStock) => {
+     const remainingStock = portfolio.filter(portfolioStock => portfolioStock.id !== soldStock.id)
+     setPortfolio(remainingStock)
+   }
+  
   return (
     <div>
-      <SearchBar />
+      <SearchBar 
+      onFilter = {setFilter}
+      onSort = {setSortMethod}
+      sortMethod = {sortMethod}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={sorting(stocks)} onStock={buyStock}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolio={portfolio} onStock={sellStock}/>
         </div>
       </div>
     </div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({stocks,onStock}) {
+  const displayStocks = stocks.map((stock)=>{
+    return <Stock key={stock.id} stock={stock} onStock={onStock}/>
+  })
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {displayStocks}
     </div>
   );
 }
